00ccce6938a4bda4673fdd5f6f77948f01d2c104
Jainish Gupta <110037836+jainishAAC@users.noreply.github.com>
2022-08-03 18:25:10 +0530

    Stable

---
+++ Assets/Scripts/MarkerManager.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerManager : MonoBehaviour
+{
+    //============================================================================================================
+    // Marker Takes The Position And Rotations
+    //============================================================================================================
+    public class Marker
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public Marker(Vector3 pos, Quaternion rot)
+        {
+            position = pos;
+            rotation = rot;
+        }
+    }
+
+    //============================================================================================================
+    // List MarkerManager(Stores Position And Rotations Of The Snake Body)
+    //============================================================================================================
+    public List<Marker> markerList = new List<Marker>();
+
+
+
+    //============================================================================================================
+    // Triggers On Every Farames in The Game
+    //============================================================================================================
+    void FixedUpdate()
+    {
+        UpdateMarkerList();
+    }
+
+    //============================================================================================================
+    // Updates The Body Positions And Rotations in The List
+    //============================================================================================================
+    public void UpdateMarkerList()
+    {
+        markerList.Add(new Marker(transform.position, transform.rotation));
+    }
+
+    //============================================================================================================
+    // Clears List Of The Body Positions And Rotations
+    //============================================================================================================
+    public void ClearMarkerList()
+    {
+        markerList.Clear();
+        markerList.Add(new Marker(transform.position, transform.rotation));
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,22 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+
+    List<GameObject> PlayerList = new List<GameObject>();
+    List<GameObject> BodyList = new List<GameObject>();
+
+
 
     [SerializeField] float Speed = 30;
     [SerializeField] float SteerVal = 700;
-    [SerializeField] float JumpVal = 700;
+    [SerializeField] GameObject body;
+    float countup = 0;
     public Rigidbody rb;
-    
+
 
 
     //=====================================================================================================
@@ -25,17 +32,19 @@ public class PlayerMovement : MonoBehaviour
     //=====================================================================================================
     void Update()
     {
-        
+
     }
     private void FixedUpdate()
     {
         PlayerMovementUpdate();
         PlayerController();
+        ManagePlayerList();
     }
 
 
     public void PlayerController()
     {
+        
         if (Input.GetKey(KeyCode.A))
         {
             Left();
@@ -56,5 +65,77 @@ public class PlayerMovement : MonoBehaviour
     public void Left()
     {
         rb.AddForce(-SteerVal, 0, 0);
+    }
+
+
+    void CreateBodyList()
+    {
+        MarkerManager markM = BodyList[PlayerList.Count - 1].GetComponent<MarkerManager>();
+
+        if (PlayerList.Count == 0)
+        {
+            GameObject temp1 = Instantiate(BodyList[0], transform.position, transform.rotation, transform);
+
+            if (!temp1.GetComponent<MarkerManager>())
+            {
+                temp1.AddComponent<MarkerManager>();
+            }
+
+            if (!temp1.GetComponent<Rigidbody>())
+            {
+                temp1.AddComponent<Rigidbody>();
+            }
+            PlayerList.Add(temp1);
+            BodyList.RemoveAt(0);
+        }
+
+        if (countup == 0)
+        {
+            markM.ClearMarkerList();
+        }
+
+        countup += Time.deltaTime;
+
+        if (countup >= 0)
+        {
+            GameObject temp = Instantiate(BodyList[0], markM.markerList[0].position, markM.markerList[0].rotation, transform);
+
+            if (!temp.GetComponent<MarkerManager>())
+            {
+                temp.AddComponent<MarkerManager>();
+            }
+
+            if (!temp.GetComponent<Rigidbody>())
+            {
+                temp.AddComponent<Rigidbody>();
+            }
+
+            PlayerList.Add(temp);
+            BodyList.RemoveAt(0);
+            temp.GetComponent<MarkerManager>().ClearMarkerList();
+            countup = 0;
+        }
+    }
+
+    void ManagePlayerList()
+    {
+        if (BodyList.Count > 0)
+        {
+            CreateBodyList();
+        }
+        for (int i = 0; i < PlayerList.Count; i++)
+        {
+            if (PlayerList[i] == null)
+            {
+                PlayerList.RemoveAt(i);
+
+                i = i - 1;
+            }
+        }
+        if (PlayerList.Count == 0)
+        {
+            Destroy(this);
+
+        }
     }
 }
