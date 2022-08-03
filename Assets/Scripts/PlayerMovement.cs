2cf1f73b4b5ab1793a4195493f6643a6eb740534
Jainish Gupta <110037836+jainishAAC@users.noreply.github.com>
2022-08-03 16:59:02 +0530

    StableBuild

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] float Speed = 30;
+    [SerializeField] float SteerVal = 700;
+    [SerializeField] float JumpVal = 700;
+    public Rigidbody rb;
+    
+
+
+    //=====================================================================================================
     // Start is called before the first frame update
+    //=====================================================================================================
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
+    //=====================================================================================================
     // Update is called once per frame
+    //=====================================================================================================
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        PlayerMovementUpdate();
+        PlayerController();
+    }
+
+
+    public void PlayerController()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Left();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Right();
+        }
+    }
+    public void PlayerMovementUpdate()
+    {
+        rb.velocity = transform.forward * Speed * Time.deltaTime;
+    }
+    public void Right()
+    {
+        rb.AddForce(SteerVal, 0, 0);
+    }
+    public void Left()
+    {
+        rb.AddForce(-SteerVal, 0, 0);
     }
 }
