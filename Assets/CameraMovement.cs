0c689c95d31d69f84052ca70bee48754a41ef80c
Jainish Gupta <110037836+jainishAAC@users.noreply.github.com>
2022-08-04 17:03:31 +0530

    Added Comments

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] GameObject[] CubeList;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    //=====================================================================================================
+    // Fixed Call In Every Devices
+    //=====================================================================================================
     void FixedUpdate()
     {
+        // Finding Player With Tag
         CubeList = GameObject.FindGameObjectsWithTag("Player");
         if (CubeList.Length > 0)
         {
+            //=====================================================================================================
+            // Main Camera Movement Referencing to the CubeList[0] If It Exists
+            //=====================================================================================================
             transform.position = new Vector3(transform.position.x, transform.position.y, CubeList[0].transform.position.z - 15);
+            //=====================================================================================================
         }
     }
+    //=====================================================================================================
 }
