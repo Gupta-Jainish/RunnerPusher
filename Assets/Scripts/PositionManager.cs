74e2bcf23e61a399f30bd7962dcc0280f7eabf21
Jainish Gupta <110037836+jainishAAC@users.noreply.github.com>
2022-08-05 13:46:18 +0530

    Minor Changes Related To The End

--- Assets/Scripts/PositionManager.cs
+++ Assets/Scripts/PositionManager.cs
@@ -9,6 +9,11 @@ public class PositionManager : MonoBehaviour
     [SerializeField] GameObject[] PositionList;
     [SerializeField] float GapBetween = 2;
     [SerializeField] float RightGap = 2;
+    [SerializeField] float Delay = 50;
+    int counter = 0;
+
+    private float waitTime = 2.0f;
+    private float timer = 0.0f;
 
     bool up = false;
 
@@ -103,28 +108,68 @@ public class PositionManager : MonoBehaviour
                     //=====================================================================================================
                     case 0:
                         {// Cheacks If It Is First Pics Or Not
-                            if (i == 0)
-                            {
-                                PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(0, 0, GapBetween);
-                                PositionList[i].gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
-                                Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                                PositionList[i].transform.LookAt(point);
-                                PositionList[i].GetComponent<Rigidbody>().useGravity = false;
+
+                            timer += Time.deltaTime;
+                       
+                            if(counter < PositionList.Length / 3)
+                                {
+                                if (timer > waitTime)
+                                {
+                                    if (i == 0)
+                                    {
+                                        Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(0, 0, GapBetween);
+                                        PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
+                                        PositionList[i].gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
+                                        Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
+                                        PositionList[i].transform.LookAt(point);
+                                        PositionList[i].GetComponent<Rigidbody>().useGravity = false;
 
 
-                            }
-                            else
-                            {
+                                    }
+                                    else
+                                    {
+                                        Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, -2, 0);
+                                        PositionList[i].transform.position = Vector3.Slerp(PositionList[i-1].transform.position, A, Delay);
+                                        PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
+                                        Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
+                                        PositionList[i].transform.LookAt(point);
+                                        PositionList[i].GetComponent<Rigidbody>().useGravity = false;
+
+                                    }
+                                    timer = 0;
+                                    counter++;
+                                }
                                 
-                                    PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, -2, 0);
+                            }
+                            if (counter >= PositionList.Length / 3)
+                            {
+                                if (i == 0)
+                                {
+                                    Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(0, 0, GapBetween);
+                                    PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
+                                    PositionList[i].gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
+                                    Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
+                                    PositionList[i].transform.LookAt(point);
+                                    PositionList[i].GetComponent<Rigidbody>().useGravity = false;
+
+
+                                }
+                                else
+                                {
+                                    Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, -2, 0);
+                                    PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
                                     PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
                                     Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
                                     PositionList[i].transform.LookAt(point);
                                     PositionList[i].GetComponent<Rigidbody>().useGravity = false;
 
+                                }
+                                timer = 0;
+                                counter++;
                             }
-                            
-                           
+
+
+
                             break;
 
                         }
@@ -135,8 +180,8 @@ public class PositionManager : MonoBehaviour
                     //=====================================================================================================
                     case 1:
                         {
-                            PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, 0, 0);
-                            PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
+                            Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, 0, 0);
+                            PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
                             Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
                             PositionList[i].transform.LookAt(point);
                             PositionList[i].GetComponent<Rigidbody>().useGravity = false;
@@ -150,8 +195,8 @@ public class PositionManager : MonoBehaviour
                     //=====================================================================================================
                     case 2:
                         {
-                            PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(2 * (RightGap), 0, 0);
-                            PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
+                            Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(2 * (RightGap), 0, 0);
+                            PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
                             Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
                             PositionList[i].transform.LookAt(point);
                             PositionList[i].GetComponent<Rigidbody>().useGravity = false;
