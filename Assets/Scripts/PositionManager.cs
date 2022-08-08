6c41611bf8d2d5fb4a1bfda83fd39de5eb75b853
Jainish Gupta <110037836+jainishAAC@users.noreply.github.com>
2022-08-08 10:55:42 +0530

    Positioning

--- Assets/Scripts/PositionManager.cs
+++ Assets/Scripts/PositionManager.cs
@@ -10,10 +10,10 @@ public class PositionManager : MonoBehaviour
     [SerializeField] float GapBetween = 2;
     [SerializeField] float RightGap = 2;
     [SerializeField] float Delay = 50;
-    int counter = 0;
 
     private float waitTime = 2.0f;
     private float timer = 0.0f;
+    int i = 0;
 
     bool up = false;
 
@@ -34,189 +34,17 @@ public class PositionManager : MonoBehaviour
     public void PosiManager()
     {
         PositionList = GameObject.FindGameObjectsWithTag("Player");
-        for (int i = 1; i < PositionList.Length; i++)
+
+        for (i = 1;i<PositionList.Length;i++)
         {
-            if (up == false)
-            {
-                mod = i % 3;
-
-                switch (mod)
-                {
-                    //=====================================================================================================
-                    // For Center Pics
-                    //=====================================================================================================
-                    case 0:
-                        {
-
-                            // Cheacks If It Is First Pics Or Not
-                            if (i == 0)
-                            {
-                                PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(0, 0, GapBetween);
-                                PositionList[i].gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
-                                Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                                PositionList[i].transform.LookAt(point);
-
-                            }
-                            else
-                            {
-                                PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, 0, GapBetween);
-                                PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
-                                Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                                PositionList[i].transform.LookAt(point);
-                            }
-                            break;
-
-                        }
-                    //=====================================================================================================    
-
-                    //=====================================================================================================
-                    // For Left Pics
-                    //=====================================================================================================
-                    case 1:
-                        {
-                            PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, 0, 0);
-                            PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
-                            Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                            PositionList[i].transform.LookAt(point);
-                            break;
-                        }
-                    //=====================================================================================================    
-
-                    //=====================================================================================================
-                    // For Right Pics
-                    //=====================================================================================================
-                    case 2:
-                        {
-                            PositionList[i].gameObject.transform.position = PositionList[i - 1].gameObject.transform.position - new Vector3(2 * (RightGap), 0, 0);
-                            PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
-                            Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                            PositionList[i].transform.LookAt(point);
-                            break;
-                        }
-                        //=====================================================================================================
-
-                }
-            }
-            if (up == true)
-            {
-                mod = i % 3;
-
-                switch (mod)
-                {
-                    //=====================================================================================================
-                    // For Center Pics
-                    //=====================================================================================================
-                    case 0:
-                        {// Cheacks If It Is First Pics Or Not
-
-                            timer += Time.deltaTime;
-                       
-                            if(counter < PositionList.Length / 3)
-                                {
-                                if (timer > waitTime)
-                                {
-                                    if (i == 0)
-                                    {
-                                        Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(0, 0, GapBetween);
-                                        PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
-                                        PositionList[i].gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
-                                        Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                                        PositionList[i].transform.LookAt(point);
-                                        PositionList[i].GetComponent<Rigidbody>().useGravity = false;
-
-
-                                    }
-                                    else
-                                    {
-                                        Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, -2, 0);
-                                        PositionList[i].transform.position = Vector3.Slerp(PositionList[i-1].transform.position, A, Delay);
-                                        PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
-                                        Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                                        PositionList[i].transform.LookAt(point);
-                                        PositionList[i].GetComponent<Rigidbody>().useGravity = false;
-
-                                    }
-                                    timer = 0;
-                                    counter++;
-                                }
-                                
-                            }
-                            if (counter >= PositionList.Length / 3)
-                            {
-                                if (i == 0)
-                                {
-                                    Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(0, 0, GapBetween);
-                                    PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
-                                    PositionList[i].gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
-                                    Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                                    PositionList[i].transform.LookAt(point);
-                                    PositionList[i].GetComponent<Rigidbody>().useGravity = false;
-
-
-                                }
-                                else
-                                {
-                                    Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, -2, 0);
-                                    PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
-                                    PositionList[i].gameObject.transform.rotation = PositionList[i - 1].gameObject.transform.rotation;
-                                    Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                                    PositionList[i].transform.LookAt(point);
-                                    PositionList[i].GetComponent<Rigidbody>().useGravity = false;
-
-                                }
-                                timer = 0;
-                                counter++;
-                            }
-
-
-
-                            break;
-
-                        }
-                    //=====================================================================================================    
-
-                    //=====================================================================================================
-                    // For Left Pics
-                    //=====================================================================================================
-                    case 1:
-                        {
-                            Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(-RightGap, 0, 0);
-                            PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
-                            Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                            PositionList[i].transform.LookAt(point);
-                            PositionList[i].GetComponent<Rigidbody>().useGravity = false;
-
-                            break;
-                        }
-                    //=====================================================================================================    
-
-                    //=====================================================================================================
-                    // For Right Pics
-                    //=====================================================================================================
-                    case 2:
-                        {
-                            Vector3 A = PositionList[i - 1].gameObject.transform.position - new Vector3(2 * (RightGap), 0, 0);
-                            PositionList[i].transform.position = Vector3.Slerp(PositionList[i - 1].transform.position, A, Delay);
-                            Vector3 point = PositionList[i].transform.position - PositionList[i - 1].transform.position;
-                            PositionList[i].transform.LookAt(point);
-                            PositionList[i].GetComponent<Rigidbody>().useGravity = false;
-
-                            break;
-                        }
-                        //=====================================================================================================
-
-                }
-            }
-            
+            PositionList[i].transform.position = PositionList[i-1].transform.position - new Vector3(0,0,GapBetween);
+        }
 
             //=====================================================================================================
             // Sets Rotations Uniform And Front Towards Z Axis Of All The Pics
             //=====================================================================================================
-            PositionList[i].transform.rotation = new Quaternion(0, 0, 0, 0);
+            // PositionList[i].transform.rotation = new Quaternion(0, 0, 0, 0);
             //=====================================================================================================
-
-
-        }
     }
     //=====================================================================================================
 
